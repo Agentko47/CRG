04dc36f64189fff1de430e68defbf91238623d01
Agentko47 <91588884+Agentko47@users.noreply.github.com>
2021-10-20 09:23:26 +0200

    Add files via upload

---
+++ IF/IF.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	/*
+		int vek = 25;
+		if (vek >= 18) {
+		cout << "Vstup povoleny" << endl;
+		 }
+	  cout << "Toto sa stane vždy"; */
+	  /*
+	  int cislo = 4;
+
+	  if (cislo == 1) {
+		  cout << "Cislo je 1";
+	  }
+	  else if (cislo == 2) {
+		  cout << "Cislo je 2";
+	  }
+	  else if (cislo == 3) {
+		  cout << "Cislo je 3";
+	  }
+	  else if (cislo == 4) {
+		  cout << "Cislo je 4";
+	  }
+	  else {
+		  cout << "Zadal si ine cislo" << endl;
+	  }
+
+	  cout << endl << "---------" << endl;
+  }
+  */
+
+	
+	const string google = "OKGoogle";
+	const string siri = "HeySiri";
+	string prikaz;
+	cin >> prikaz;
+	if (prikaz == google ||prikaz == siri) {
+		cout << "Zadal si spravny prikaz" << endl;
+	}
+	int hlasitost;
+	cout << "Zadaj hlasitost" << endl;
+	cin >> hlasitost;
+	if (hlasitost <= 100);
+	
+
+}
+
